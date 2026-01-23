dd451ac65ea30cc21af04ada53ade0b976b3407a
skookumer <ruminator@DESKTOP-41LQTGQ.localdomain>
2026-01-23 12:42:13 -0500

    made it fancier

--- Project01/eric_script.py
+++ Project01/eric_script.py
@@ -1,35 +1,40 @@
 from pathlib import Path
 import re
+from collections import defaultdict
 
 home = Path(__file__).parent
 path = home / "clinvar_20190923_short.vcf"
 
 
 def parse_function(text, threshold_key, retrieval_key, threshold=0.0001):
-    target_pattern = re.compile(rf'{threshold_key}=([^;]+)')
+    '''
+    function parses the line and looks for specific keywords using regex, 
+    then returns what is between the equal sign and the next semicolon
+    '''
+    target_pattern = re.compile(rf'{threshold_key}=([^;]+)')                # compiling patterns
     retrieval_pattern = re.compile(rf'{retrieval_key}=([^;]+)')
-    if re.search(target_pattern, text):
+    if re.search(target_pattern, text):                                     # check if target exists
         value = target_pattern.search(text)
         value = float(value.group(1))
-        if value < threshold and re.search(retrieval_pattern, text):
+        if value < threshold and re.search(retrieval_pattern, text):        # check if retrieval exists and if value < threshold
             result = retrieval_pattern.search(text)
             result = result.group(1)
-            return result.split("|")
+            return result.split("|")                                        # return the list split by vertical bars
     return []
 
 
-def read_file(path, avoid = {"not_provided", "not_specified"}):
-    unique = {}
+def read_file(path, avoid={"not_provided", "not_specified"}):
+    '''
+    scroll thru the lines of the file and read them one by one
+    add unique items to dict and count them
+    '''
+    unique = defaultdict(int)                                               # use defaultdict to initialize values
     for line in open(path, "r", encoding="utf-8"):
         dn_vals = parse_function(line, "AF_EXAC", "CLNDN")
-        if len(dn_vals) > 0:
-            for dn in dn_vals:
-                if dn not in avoid:
-                    if dn in unique:
-                        unique[dn] += 1
-                    else:
-                        unique[dn] = 1
-    return unique
+        for dn in dn_vals:
+            unique[dn] += 1
+    [unique.pop(key) for key in avoid]                                      # remove unwanted values
+    return dict(unique)                                                     # cast as dict for return
     
 
 results = read_file(path)
